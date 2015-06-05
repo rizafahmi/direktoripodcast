f5412278651adf427d2cc82bc37c2245cbc5e148
Riza Fahmi <rizafahmi@gmail.com>
2015-06-05 15:08:02 +0700

    Add producer

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,18 +7,22 @@ defmodule DirektoriPodcast.PageController do
     cards = [
       %{title: "AppsCoast: Indonesian Tech Startup Podcast",
         description: "Radio-quality Tech Startup Podcast",
-        url: "https://soundcloud.com/amp-asia/sets/appscoast"
+        url: "https://soundcloud.com/amp-asia/sets/appscoast",
+        producer: "AMPlified"
         },
       %{title: "Doujin Dalam Botol",
         description: "Ini adalah kanal podcast resmi dari Doujin Dalam Botol, sebuah perusahaan yang bergerak di bidang distribusi dan pengiklanan karya-karya Dōjinshi di Indonesia.",
+        producer: "DoujinDalamBotol",
         url: "https://soundcloud.com/doujindalambotol"
         },
       %{title: "Marketeers Podcast",
         description: "",
+        producer: "Marketeers",
         url: "https://soundcloud.com/marketeers-podcast"
         },
       %{title: "Hai Dokter Radio",
         description: "Hold on tight, Healthy People!  HaiDokter Media will be coming soon.",
+        producer: "AMPlified",
         url: "https://soundcloud.com/search/sounds?q=haidokter"
         },
     ]
